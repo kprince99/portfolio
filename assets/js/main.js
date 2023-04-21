84e96ac9b9794cc7bc919562585becd885bc6593
Prince Kumar <58019458+kprince99@users.noreply.github.com>
2023-04-21 18:22:52 +0530

    style update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+      navClose = document.getElementById("nav-close"),
+      navToggle = document.getElementById("nav-toggle");
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    });
+
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
 
 /*============== MENU HIDDEN ===============*/
 
